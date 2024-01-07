97980d2a6e0730ef76a5643840e9f3684abed7f8
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-01-07 21:59:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+let name ="shubham";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <>
+
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contacts</li>
+    </nav>
+
+    <div className="container">
+      <h1>
+        Hello {name}
+      </h1>
+      <p>
+        i am a paragraph
+      </p>
     </div>
+
+    
+
+    
+
+
+    </>
+
+
+
+  
   );
 }
 
